32ec27564b32120f1d2aa7e2cb9ff24b6e6beabf
sabrinafrazao <sabrinadasilvafrazao@gmail.com>
2024-09-22 21:14:26 -0400

    Adicionado produtos na API e terminando de configurar a api para o crud de produtos

--- api_database/repository/script_db/script.sql
+++ api_database/repository/script_db/script.sql
@@ -9,14 +9,6 @@ GRANT ALL PRIVILEGES ON banco.* TO 'root' @'localhost';
 -- Acesar o banco de dados: banco
 USE banco;
 -- Criar a tabela: usuario
-CREATE TABLE usuario(
-    id int AUTO_INCREMENT,
-    nome varchar(50) NOT NULL,
-    login varchar(20) NOT NULL,
-    senha varchar(20) NOT NULL,
-    email varchar(50) NOT NULL,
-    PRIMARY KEY (id)
-);
 
 CREATE TABLE produto(
     id int AUTO_INCREMENT,
